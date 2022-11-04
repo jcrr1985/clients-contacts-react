c5fac34ea620e9059e99ecc4b4a1e989fd7e80c4
Julio Rodriguez <julio.rodriguezr1@gmail.com>
2022-11-04 06:53:27 -0500

    chore: setup mock service worker

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import { StyledEngineProvider } from '@mui/material/styles'
 
 import theme from './theme'
 import App from '@/pages/supplier/new-supplier'
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = await import('./mocks/supplier-api')
+  worker.start()
+}
 
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement)
