c0002c8b5898ddd03b2221f1e8935e43e9054629
Julio Rodriguez <julio.rodriguezr1@gmail.com>
2022-11-04 07:00:21 -0500

    remove: react strict mode wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import { StyledEngineProvider } from '@mui/material/styles'
 
 import theme from './theme'
-import App from '@/pages/supplier/new-supplier'
+import NewSupplierPage from '@/pages/supplier/new-supplier'
+
 if (process.env.NODE_ENV === 'development') {
   const { worker } = await import('./mocks/supplier-api')
   worker.start()
@@ -14,12 +14,10 @@ if (process.env.NODE_ENV === 'development') {
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement)
 root.render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <CssBaseline />
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </StyledEngineProvider>
-  </React.StrictMode>,
+  <StyledEngineProvider injectFirst>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <NewSupplierPage />
+    </ThemeProvider>
+  </StyledEngineProvider>,
 )
