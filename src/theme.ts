3368f211b8b680d6dc23e18e74b4b8ca2b156356
Julio Rodriguez <julio.rodriguezr1@gmail.com>
2022-11-08 14:37:58 -0500

    chore: sets theme primary color

--- src/theme.ts
+++ src/theme.ts
@@ -1,26 +1,9 @@
 import { createTheme } from '@mui/material/styles'
 
-const rootElement = document.getElementById('root')
-
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#3F8AE0',
-    },
-    secondary: {
-      main: '#326eb3',
-    },
-  },
-  components: {
-    MuiPopover: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-    MuiPopper: {
-      defaultProps: {
-        container: rootElement,
-      },
+      main: '#ee7302',
     },
   },
 })
